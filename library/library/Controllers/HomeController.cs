f85b6c41f445b8fa926bd6a2271f723d97daed34
SLAVA747 <43703034+SLAVA747@users.noreply.github.com>
2018-12-18 06:11:37 +0700

    Добавлен список читателей
    Добавлен список читателей

--- library/library/Controllers/HomeController.cs
+++ library/library/Controllers/HomeController.cs
@@ -15,16 +15,16 @@ namespace library.Controllers
 
             return View();
         }
+        public ActionResult readers()
+        {
+
+
+            return View();
+        }
         [HttpGet]
-        public JsonResult Index2()
+        public JsonResult Index_library()
         {
             library_globalContext db = new library_globalContext();
-            /* var num_info = (from a in db.Numbers
-                             join b in db.Company on a.CompanyId equals b.CompanyId
-                             select new
-                             {
-                                 bla1 = b.CompanyName
-                             }).ToList();*/
             var num_info = (from a in db.Полка
                             join b in db.Авторы on a.IdАвтора equals b.IdАвтора
                             join c in db.Жанры on a.IdЖанра equals c.IdЖанра
@@ -35,6 +35,25 @@ namespace library.Controllers
                                 Название_книги = d.НазваниеКниги,
                                 ФИО_Автора = b.Фио,
                                 Жанр = c.НазваниеЖанра
+                            }).ToList();
+            return Json(num_info, JsonRequestBehavior.AllowGet);
+        }
+        [HttpGet]
+        public JsonResult Index_readers()
+        {
+            library_globalContext db = new library_globalContext();
+            var num_info = (from a in db.Читатели
+                            join b in db.Клиенты on a.IdКлиента equals b.IdКлиента
+                            join c in db.ВидыКонтактов on a.IdВидКонтакта equals c.IdВидКонтакта
+                            select new
+                            {
+                                Id_читателя = a.IdЧитателя,
+                                Фамилия = b.Фамилия,
+                                Имя = b.Имя,
+                                Отчество = b.Отчество,
+                                Вид_контакта = c.Наименование,
+                                контактные_данные = a.КонтактныеДанные,
+                                адрес = b.Адрес
                             }).ToList();
             return Json(num_info, JsonRequestBehavior.AllowGet);
         }
