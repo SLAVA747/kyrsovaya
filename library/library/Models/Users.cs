34d268ce17f4cb22a98ca94f882603fe36bfca91
SLAVA747 <43703034+SLAVA747@users.noreply.github.com>
2018-12-18 16:41:05 +0700

    Подключил базу + добавил форму авторизации

---
+++ library/library/Models/Users.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace library.Models
+{
+    public partial class Users
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ library/library/Models/usersContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace library.Models
+{
+    public partial class usersContext : DbContext
+    {
+        public usersContext()
+        {
+        }
+
+        public usersContext(DbContextOptions<usersContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Users> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseNpgsql("Host=localhost;Database=users;Username=postgres;Password=1234567890");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.0-rtm-35687");
+
+            modelBuilder.Entity<Users>(entity =>
+            {
+                entity.ToTable("users");
+
+                entity.Property(e => e.Id)
+                    .HasColumnName("id")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.Login).HasColumnName("login");
+
+                entity.Property(e => e.Password).HasColumnName("password");
+            });
+        }
+    }
+}
